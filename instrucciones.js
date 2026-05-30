60bb5066ffe75009e0bdd9e5b6ac9332c79b252e
Knight77 <logicmedia7775@gmail.com>
2026-05-29 18:05:27 -0600

    juego  flappy bird

---
+++ instrucciones.js
@@ -0,0 +1,288 @@
+const jugador =
+document.getElementById(
+"jugador"
+);
+
+const tuboSup1 =
+document.getElementById(
+"tuboSuperior1"
+);
+
+const tuboInf1 =
+document.getElementById(
+"tuboInferior1"
+);
+
+const tuboSup2 =
+document.getElementById(
+"tuboSuperior2"
+);
+
+const tuboInf2 =
+document.getElementById(
+"tuboInferior2"
+);
+
+
+
+const puntajeTexto =
+document.getElementById(
+"puntaje"
+);
+
+
+
+/* variables */
+
+let y = 200;
+
+let gravedad = 3;
+
+let salto = 45;
+
+let tuboX1 = window.innerWidth;
+let tuboX2 = window.innerWidth + 400;
+
+let puntos = 0;
+
+let juegoActivo = true;
+
+/* salto */
+
+function brincar(){
+
+if(juegoActivo){
+
+y -= salto;
+
+}
+
+}
+
+/* teclado */
+
+document.addEventListener(
+"keydown",
+
+function(event){
+
+if(
+event.code=="Space"
+){
+
+brincar();
+
+}
+
+}
+
+);
+
+/* click móvil */
+
+document.addEventListener(
+"click",
+
+brincar
+
+);
+
+/* loop */
+
+function actualizar(){
+
+if(
+!juegoActivo
+){
+
+return;
+
+}
+
+/* gravedad */
+
+y += gravedad;
+
+jugador.style.top =
+y + "px";
+
+/* movimiento tubos */
+
+tuboX1 -= 5;
+tuboX2 -= 5;
+
+tuboSup1.style.left =
+tuboX1 + "px";
+
+tuboInf1.style.left =
+tuboX1 + "px";
+
+tuboSup2.style.left =
+tuboX2 + "px";
+
+tuboInf2.style.left =
+tuboX2 + "px";
+
+/* reinicio tubos */
+
+
+if(
+tuboX1 < -100
+){
+
+tuboX1 =
+window.innerWidth;
+
+let altura1 =
+Math.floor(
+Math.random()*350
+)+50;
+
+let hueco = 200;   
+
+tuboSup1.style.height =
+altura1 + "px";
+
+tuboInf1.style.height =
+(window.innerHeight-altura1-hueco)+"px";
++ "px";
+
+puntos++;
+puntajeTexto.innerHTML =
+"Puntos: " + puntos;
+}
+
+if(
+tuboX2 < -100
+){
+
+tuboX2 =
+window.innerWidth;
+
+let altura2 =
+Math.floor(
+Math.random()*200
+)+100;
+
+tuboSup2.style.height =
+altura2 + "px";
+
+tuboInf2.style.height =
+(450-altura2)
++ "px";
+
+puntos++;
+
+puntajeTexto.innerHTML =
+"Puntos: " + puntos;
+
+}
+
+/* colisión suelo */
+
+if(
+y > window.innerHeight-60
+){
+
+finJuego();
+
+}
+
+/* colisión techo */
+
+if(
+y < 0
+){
+
+finJuego();
+
+}
+
+/* colisión tubos */
+
+let jugadorRect =
+jugador.getBoundingClientRect();
+
+
+
+if(
+
+intersecta(
+jugadorRect,
+tuboSup1.getBoundingClientRect()
+
+)
+
+||
+
+intersecta(
+jugadorRect,
+tuboInf1.getBoundingClientRect()
+)
+
+||
+
+intersecta(
+jugadorRect,
+tuboSup2.getBoundingClientRect()
+)
+
+||
+
+intersecta(
+jugadorRect,
+tuboInf2.getBoundingClientRect()
+)
+
+){
+
+finJuego();
+
+}
+}
+
+/* detección */
+
+function intersecta(
+a,
+b
+){
+
+return !(
+a.right < b.left ||
+
+a.left > b.right ||
+
+a.bottom < b.top ||
+
+a.top > b.bottom
+
+);
+
+}
+
+/* fin */
+
+function finJuego(){
+
+juegoActivo = false;
+
+alert(
+"Juego terminado\nPuntos: "
++
+puntos
+);
+
+location.reload();
+
+}
+
+/* velocidad */
+
+setInterval(
+
+actualizar,
+
+25
+
+);
